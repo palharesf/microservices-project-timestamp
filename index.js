015ef45a2df86f80c2f8b72bbd7f8c8828429697
Fernando <palhares.f@gmail.com>
2024-11-20 14:55:35 -0500

    Built core function; correctly handling empty date, invalid date, and UTC dates

--- index.js
+++ index.js
@@ -18,15 +18,49 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
-
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 4000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+
+
+// All before this was boilerplate; below is the code for the challenge
+
+// Core function: api/:date? , where :date? can be accessed via req.params.date
+app.get("/api/:date?", function (req, res) {
+  var date = req.params.date;
+
+  // Check if a date was passed, otherwise return current time
+  if (!date) {
+    res.json(
+      {
+        unix: new Date().getTime(),
+        utc: new Date().toUTCString()
+      }
+    )
+  }
+
+  // Check if date passed was valid
+  if (new Date(date).toString() === "Invalid Date") {
+    res.json(
+      {
+        error: "Invalid Date"
+      }
+    )
+  } else {
+    res.json(
+      {
+        unix: new Date(date).getTime(),
+        utc: new Date(date).toUTCString()
+      }
+    )
+  }  
+});
+
+
